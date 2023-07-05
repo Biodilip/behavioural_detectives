dcb3aa69ae693a2cb4614217690dedaddd7a2707
cammynaf@hotmail.com <cammynaf@hotmail.com>
2023-07-05 18:55:25 +0200

    updates

--- Code/behaviouralODEs.r
+++ Code/behaviouralODEs.r
@@ -17,7 +17,6 @@ library(ggplot2)
 
 rm(list=ls())                   # Clear all variables and functions
 
-<<<<<<< HEAD
 setwd("C:/Users/corlendo/behavioural_detectives")
 source("code/hetero_lambda.r")
 source("code/behaviour_mort_effect.r")
@@ -25,31 +24,15 @@ source("code/both_effects.r")
 source("code/box_car_infections.r")
 source("code/HIV_SI_ODEs.r")
 
-=======
-source("hetero_lambda.r")
-source("behaviour_mort_effect.r")
-source("both_effects.r")
-source("box_car_infections.r")
->>>>>>> 66c355e48fe982936e9c866e2c41aa3412a3949b
-## Our state variables
-## 
-## S(t) - The number of susceptible adults over the age of 25 years at time t
-## I(t) - The number of infected adults over the age of 25 years at time t
-## with N(t) = S(t) + I(t)
 
-## and parameters:
-##
-## N - the total population size; N = S + I [individuals]
-## bRt - per capita birth rate [1 / time]
-## dRt - per capita mortality rate
-## Beta - transmission coefficient when prevalence is 0 
-##alpha - for transmission coefficient: decline with prevalence
+## Script that explores the effect of heterogeneity or behaviour on model 
 
-#Disease parameters
+
+#1) Define the disease parameters
 ## Function that makes a list of disease parameters with default values
-disease_params <- function(Beta = 0.9 ## transmission coefficient when prevalence is 0 
+disease_params <- function(Beta = 0.2 ## transmission coefficient when prevalence is 0 
                            , alpha = 8 ## for transmission coefficient: decline with prevalence
-                           , n = 6 ## number of box  cars
+                           , n = 3 ## number of box  cars
                            , pRt = (1/10)*n ## rate of of progression through each of the I classes, for 10 years total
                            , bRt = 1/60 ## birth rate = death rates in a closed popn
                            , dRt = 1/60 ## 60 year natural life expectancy
@@ -73,16 +56,16 @@ time.out <- seq(0, 365, 1)
 SI.ts <- data.table(lsoda(
   y = pop.SI0,               # Initial conditions for population
   times = time.out,             # Timepoints for evaluation
-  func = HIV_SI,                   # Function to evaluate
+  func = HIV_SI_ODEs,                   # Function to evaluate
   parms = disease_params()               # Vector of parameters
 ))
 
 
 
 SI.ts$I <- rowSums(SI.ts[,3:(k+2)])
-#SI.ts <- SI.ts[,-3:-(k+2)]
-SI.ts$N <- rowSums(SI.ts[,3:ncol(SI.ts)])
-SI.ts[, P := I / N]
+SI.ts <- SI.ts[,-3:-(k+2)]
+SI.ts$N <- SI.ts[,I] + SI.ts[,S]
+SI.ts[, P := CI / N]
 
 #plot output
 SI.ts.long <- melt(SI.ts, id.vars = 'time')
