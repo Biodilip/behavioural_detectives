00fe036635f9b33a5aa876c8524ea40c4b529ef7
cammynaf@hotmail.com <cammynaf@hotmail.com>
2023-07-05 18:52:58 +0200

    Update of ODEs

---
+++ Code/HIV_SI_ODEs.r
@@ -0,0 +1,29 @@
+## Our state variables
+## 
+## S(t) - The number of susceptible adults over the age of 25 years at time t
+## I(t) - The number of infected adults over the age of 25 years at time t
+## with N(t) = S(t) + I(t)
+
+##-- HIV - SI model from --Hargrove, John W., et al. 
+#"Declining HIV prevalence and incidence in perinatal women in Harare, Zimbabwe." Epidemics 3.2 (2011): 88-94.
+HIV_SI_ODEs <- function(t,y,parms,n){
+  with(c(as.list(y),parms),{
+    #N <- sum(y)
+    
+    I_vec <- y[2:(n+1)]
+    I <-  sum(I_vec)          ## total infected
+    N <- I + S                 ## total population
+    lambda <- hetero_lambda(Beta,alpha,I,N)
+    #lambda <- behaviour_mort_effect(Beta,alpha,I,N,n,tail(I_vec,n=1),q)
+    #lambda <- both_effects(Beta,alpha,I,N,n,tail(I_vec,n=1),q)
+    ## state variable derivatives (ODE system)
+    
+    deriv <- rep(NA,3+(n)) # number of derivatives
+    deriv[1] <-	bRt*N - (lambda+dRt)*S  ## Instantaneous rate of change: Susceptibles
+    deriv[2:(n+1)] <- box_car_infections(S, I_vec,lambda, dRt,pRt)
+    deriv[(length(deriv)-1)] <-	(lambda)*S ## Instantaneous rate of change: Cumulative incidence
+    deriv[length(deriv)] <-	pRt*deriv[n+1] ## Instantaneous rate of change: Cumulative mortality
+    return(list(deriv))
+  })
+}
+
--- Code/behaviouralODEs.r
+++ Code/behaviouralODEs.r
@@ -22,6 +22,8 @@ source("code/hetero_lambda.r")
 source("code/behaviour_mort_effect.r")
 source("code/both_effects.r")
 source("code/box_car_infections.r")
+source("code/HIV_SI_ODEs.r")
+
 ## Our state variables
 ## 
 ## S(t) - The number of susceptible adults over the age of 25 years at time t
@@ -40,11 +42,11 @@ source("code/box_car_infections.r")
 ## Function that makes a list of disease parameters with default values
 disease_params <- function(Beta = 0.9 ## transmission coefficient when prevalence is 0 
                            , alpha = 8 ## for transmission coefficient: decline with prevalence
-                           , n = 2 ## number of box  cars
+                           , n = 6 ## number of box  cars
                            , pRt = (1/10)*n ## rate of of progression through each of the I classes, for 10 years total
                            , bRt = 1/60 ## birth rate = death rates in a closed popn
                            , dRt = 1/60 ## 60 year natural life expectancy
-                           , q = 50 # effect of behaviour change on mortality
+                           , q = 150 # effect of behaviour change on mortality
 ){
   return(as.list(environment()))
 }
@@ -59,31 +61,6 @@ pop.SI0 <- c(S=1-initPrev,
              I_vec, CI = 0, 
              CD = 0)      #Initial conditions
 time.out <- seq(0, 365, 1)
-
-
-##-- HIV - SI model from --Hargrove, John W., et al. 
-#"Declining HIV prevalence and incidence in perinatal women in Harare, Zimbabwe." Epidemics 3.2 (2011): 88-94.
-HIV_SI <- function(t,y,parms,n){
-  with(c(as.list(y),parms),{
-    #N <- sum(y)
-    
-    I_vec <- y[2:(n+1)]
-    I <-  sum(I_vec)          ## total infected
-    N <- I + S                 ## total population
-    lambda <- hetero_lambda(Beta,alpha,I,N)
-    #lambda <- behaviour_mort_effect(Beta,alpha,I,N,n,tail(I_vec,n=1),q)
-    #lambda <- both_effects(Beta,alpha,I,N,n,tail(I_vec,n=1),q)
-    ## state variable derivatives (ODE system)
-    
-    deriv <- rep(NA,3+(n)) # number of derivatives
-    deriv[1] <-	bRt*N - (lambda+dRt)*S  ## Instantaneous rate of change: Susceptibles
-    deriv[2:(n+1)] <- box_car_infections(S, I_vec,lambda, dRt,pRt)
-    deriv[(length(deriv)-1)] <-	(lambda)*S ## Instantaneous rate of change: Cumulative incidence
-    deriv[length(deriv)] <-	pRt*deriv[n+1] ## Instantaneous rate of change: Cumulative mortality
-    return(list(deriv))
-  })
-}
-
 
 ## Solve ODE using lsoda and give our output in the data.table
 SI.ts <- data.table(lsoda(
@@ -108,7 +85,7 @@ SI.ts.long <- melt(SI.ts, id.vars = 'time')
   + geom_line()
 )
 
-(ggplot(SI.ts.long[variable %in% c('S', 'I', 'CD')])
+(ggplot(SI.ts.long[variable %in% c('S', 'I', "CI","CD","N")])
   + aes(x = time, y = value)
   + geom_line()
   + facet_wrap(~ variable)
